b6c82e08f4bff202241e72fabe0ba82afd32d541
Tamara Sibileva <tomav93@mail.ru>
2024-12-06 15:46:48 +1200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет");
+        System.out.println("2+3");
+        System.out.println(2+3);
+
+        int speed = 70;
+        int x = speed + 5;
+
+        double d = 6.5;
+
+        String name = "Vanya";
+
+        System.out.println(name + " ехал со скоростью " + x);
+
+
+    }
+}
